5f6588704f7108188ff2cae6c91bc19e840ffcd6
Rafa <rafaeldutraribas@gmail.com>
2023-03-02 17:38:01 -0300

    first commit

---
+++ studio_game.rb
@@ -0,0 +1,10 @@
+player = 'larry'
+playerHealth = 60
+
+puts "#{player}'s health is #{3 * playerHealth}"
+
+puts playerHealth/9
+puts playerHealth/9.0
+
+
+puts "Players:\n \tlarry\n \tcurly\n \tmoe"
