f0bc5822aac3d5b1f87abbaea07dc5f1daa13a79
Rafa <rafaeldutraribas@gmail.com>
2023-03-06 08:21:23 -0300

    cap 8 atributos

--- studio_game.rb
+++ studio_game.rb
@@ -1,14 +1,25 @@
 class Player
+  attr_accessor :name
+  attr_reader :health
+
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
+
+  def score
+    @health + @name.length
+  end
   def to_s
-    "I'm #{@name} with a health of #{@health}"
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 end
 
 p = Player.new("anna", 30)
-
 puts p
+
+p.name = "Lawrence"
+puts p.name
+puts p.health
+puts p.score
 
