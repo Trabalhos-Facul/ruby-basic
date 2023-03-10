e6dbe84496709f85569f11b498aec8f071b761d2
Rafa <rafaeldutraribas@gmail.com>
2023-03-09 21:48:09 -0300

    cap 13 game TDD - exercicio

--- game.rb
+++ game.rb
@@ -10,8 +10,16 @@ class Game
     @players << new_player
   end
 
+  def die_roll
+    rand(1..6)
+  end
+
   def play
     puts "There are #{@players.length} players in #{@name}}:"
-    puts @players
+    @players.each do |player|
+      player.w00t if die_roll >= 5
+      player.blam if die_roll < 3
+      puts @player
+    end
   end
 end
