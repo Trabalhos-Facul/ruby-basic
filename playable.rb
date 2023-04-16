dae1dbd93732d2cff54274c4f00e1d0eb6d4db22
Rafa <rafaeldutraribas@gmail.com>
2023-04-16 15:22:53 -0300

    cap 21 mixins execicio

---
+++ playable.rb
@@ -0,0 +1,9 @@
+module Playable
+  def w00t
+    self.health += 15
+  end
+
+  def blam
+    self.health -= 10
+  end
+end
--- player.rb
+++ player.rb
@@ -1,6 +1,7 @@
+require_relative 'playable'
 class Player
-  attr_accessor :name
-  attr_reader :health
+  include Playable
+  attr_accessor :name, :health
 
   def initialize(name, health=100)
     @name = name.capitalize
@@ -38,14 +39,6 @@ class Player
 
   def to_s
     "I'm #{@name} with a health = #{@health}, points = #{points}, and a score = #{score}."
-  end
-
-  def w00t
-    @health += 15
-  end
-
-  def blam
-    @health -= 10
   end
 
   def found_treasure(treasure)
